197a390e95cc428f6e8a49b579c1b19dd18a7ab4
aleksandrs <soskola@ad.hva.nl>
2026-01-16 20:23:11 +0100

    feat: SinglyLinkedList

--- src/main/java/graphs/SinglyLinkedList.java
+++ src/main/java/graphs/SinglyLinkedList.java
@@ -1,26 +1,32 @@
 package graphs;
 
-
 import java.util.Iterator;
 
 /**
- * A doubly ended singly linked list.
- * Supports adding elements at the front and end,
- * retrieving by index, checking size, and iterating over elements.
+ * A doubly ended singly linked list. Supports adding elements at the front and
+ * end, retrieving by index, checking size, and iterating over elements.
  *
  * @param <E> the type of elements stored in the list
  */
 public class SinglyLinkedList<E> implements Iterable<E> {
+
     private Node<E> head;       // first node containing the first item in the linked list
     private Node<E> tail;       // last node containing the last item in the linked list
     private int size;           // current number of items in the linked list
 
-
     /**
-     * Inner class representing a node in the singly linked list.
+     * Inner class representing a node in the singly linked list.\ Singlely
+     * linked lists only move in one direction.
      */
     private static class Node<E> {
-        // TODO: add necessary attributes (data, next reference) & a constructor
+
+        // Defines data to hold value and "next" to point to the next node.
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
     }
 
     public SinglyLinkedList() {
@@ -35,8 +41,18 @@ public class SinglyLinkedList<E> implements Iterable<E> {
      * @param item the element to add
      */
     public void add(E item) {
-        // TODO: create a new node and attach it at the end of the list
-        // TODO: increment size
+        // Creates new node and updates the tail (and head if empty).
+        Node<E> newNode = new Node<>(item);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        // Tracks list length
+        size++;
     }
 
     /**
@@ -45,7 +61,17 @@ public class SinglyLinkedList<E> implements Iterable<E> {
      * @param item the element to add
      */
     public void addFirst(E item) {
-        // TODO: create a new node and set it as the new head
+        // Creates new node pointing to current head and updates head reference.
+        Node<E> newNode = new Node<>(item);
+
+        if (head == null) {
+            tail = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+
+        // Tracks list length
+        size++;
     }
 
     /**
@@ -56,9 +82,18 @@ public class SinglyLinkedList<E> implements Iterable<E> {
      * @throws IndexOutOfBoundsException if index is not within range
      */
     public E get(int index) {
-        // TODO: find the node at the index position and return the item from the node
+        // Validates index bounds and traverses the list from head using a loop to find the target node.
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
 
-         return null;
+        Node<E> current = head;
+
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current.data;
     }
 
     /**
@@ -73,15 +108,30 @@ public class SinglyLinkedList<E> implements Iterable<E> {
      * Returns an iterator over the elements in this list.
      *
      * @return an {@link Iterator} over the elements in this list
-     * @throws java.util.NoSuchElementException if the {@link Iterator#next()} method is called
-     *         when no more elements are available
+     * @throws java.util.NoSuchElementException if the {@link Iterator#next()}
+     * method is called when no more elements are available
      */
     @Override
     public Iterator<E> iterator() {
-        // TODO replace below iterator by a more memory efficient, true iterator instance
-        //      that iterates along all items in the list.
+        // Custom Iterator that traverses nodes sequentially without copying data.
+        // This allows the list to be used in for each loops. This is more memory efficient (walks over the existing nodes one by one).
+        return new Iterator<E>() {
+            private Node<E> current = head;
 
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
 
-        return null;
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new java.util.NoSuchElementException();
+                }
+                E data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
     }
 }
